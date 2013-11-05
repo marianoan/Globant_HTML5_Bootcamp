816bd74ccdeb06ab662a0fc41b71123e563cb278
marianoan <marianoangonzalez@gmail.com>
2013-11-05 17:29:37 -0300

    Finish topic 7 with backbone

---
+++ Topic 7/js/collections/movies.js
@@ -0,0 +1,27 @@
+﻿define([
+  'underscore',
+  'backbone',
+  'localStorage',
+  'models/Movie',
+], function (_, Backbone, localStorage,Movie) {
+
+    var Movies = Backbone.Collection.extend({
+        model: Movie,
+
+        localStorage: new localStorage('contact_list_backbone'),
+
+        //url: "js/collection.html",
+
+        nextOrder: function () {
+            if (!this.length) {
+                return 1;
+            }
+            return this.last().get('order') + 1;
+        },
+
+
+
+    });
+    // You don't usually return a collection instantiated
+    return Movies;
+});
---
+++ Topic 7/js/models/Movie.js
@@ -0,0 +1,18 @@
+﻿define([
+  'underscore',
+  'backbone'
+], function (_, Backbone) {
+
+    var Movie = Backbone.Model.extend({
+        defaults: {
+            name: 'noname',
+            year: 'address',
+            director: 'mobile',
+            img: 'http://placehold.it/370x75',
+            cast: 'email',
+            sinopsis: 'lorem ipsum dolor'
+        }
+    });
+    // Return the model for the module
+    return Movie;
+});

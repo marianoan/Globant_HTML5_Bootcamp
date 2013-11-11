f602c10a7c1819a2a201eb141a7b2c4b6484c351
marianoan <marianoangonzalez@gmail.com>
2013-11-11 14:45:57 -0300

    Finish topic 8

---
+++ Topic 8/js/main.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+
+    $('body').addClass('js');
+
+    var $menu = $('#menu'),
+      $menulink = $('.menuLink'),
+      $wrap = $('#wrapper');
+
+    $menulink.click(function () {
+        console.log("hh");
+        $menulink.toggleClass('active');
+        $wrap.toggleClass('active');
+        return false;
+    });
+});
